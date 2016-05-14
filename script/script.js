1698f168b3f619f6c7cebf55c6c7d61f2888d9a9
Savtym <savtym@gmail.com>
2016-05-15 01:08:09 +0300

    home page release 1.1

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
-var iCur = 0;
-
+//snap menu
 var snapper = new Snap({
     element: document.getElementById('content'),
     disable: 'left'
@@ -22,16 +21,21 @@ $('.icon-search').click(function() {
     $('.search input').focus();
 });
 
+//scroll menu
 var elem = document.getElementById('content');
-
 if (elem.addEventListener) {
   if ('onwheel' in document) {
     elem.addEventListener("wheel", onWheel);
   }
 }
-
 function onWheel(e) {
   e = e || window.event;
   var delta = e.deltaY || e.detail || e.wheelDelta;
   (delta > 0) ? $('.top-bar').addClass('scroll') : $('.top-bar').removeClass('scroll');
 }
+
+
+// login form
+$('.login').click(function() {
+	($('.loginform').hasClass('active')) ? $('.loginform').removeClass('active') : $('.loginform').addClass('active animated fadeInDown');
+})
